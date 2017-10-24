0883a6e4709cbf9db58b244abcbb2672296d6b1f
Hayden Wang <haydenwang@ecoworkinc.com>
2017-10-24 14:24:10 +0800

    add instruction

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ int main()
     Client result[MAX_CLIENT];
 
     // Create client queue array.
+    cout << "Please enter time values of each client in order of: ";
+    cout << "Arrival Time, Service Time and Allowable Waiting Time." << endl;
+    cout << "And separate time values by comma(,)." << endl;
+    cout << "For example, ";
+    cout << "If the arrival time, service time and allowable waiting time of a client is: " << endl;
+    cout << "3, 5 and 7, then you should enter: '3,5,7'." << endl;
     Client client_queue[MAX_CLIENT];
     for(int i = 0; i < MAX_CLIENT; i++)
     {
