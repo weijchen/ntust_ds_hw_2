335429352526f1504e3d4538de784a36abde0bb5
Hayden Wang <haydenwang@ecoworkinc.com>
2017-10-24 08:55:24 +0800

    git init

---
+++ client.h
@@ -0,0 +1,81 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Client
+{
+public:
+    int user_id;
+    int arrival_time;
+    int service_time;
+    int allowable_waiting_time;
+    int departure_time;
+    bool is_served;
+
+    void create_client(int customer_id)
+    {
+        user_id = customer_id;
+        string time_string;
+        cout << "Enter times for user[" << customer_id << "]:";
+        cin >> time_string;
+
+        string delimiter = ",";
+        size_t pos = 0;
+        string token;
+        int count = 0;
+        int TIME_NUMBER = 3; // arrival time, service time and allowable waiting time.
+        // Split input string
+        while ((pos = time_string.find(delimiter)) != std::string::npos && count < TIME_NUMBER - 1)
+        {
+            if (count < TIME_NUMBER)
+            {
+                token = time_string.substr(0, pos);
+                if (count == 0) { arrival_time = stoi(token); }
+                else { service_time = stoi(token); }
+                time_string.erase(0, pos + delimiter.length());
+                count++;
+            }
+        }
+        allowable_waiting_time = stoi(time_string);
+    }
+
+    void print_info()
+    {
+        cout << "User: " << user_id << ", ";
+        cout << "Arrival Time: " << arrival_time << ", ";
+        cout << "Service Time: " << service_time << ", ";
+        cout << "Allowable Waiting Time: " << allowable_waiting_time << endl;
+    }
+
+    void print_result()
+    {
+        string served_result;
+        if (is_served == true) { served_result = "Yes"; }
+        else { served_result = "No"; }
+
+        cout << "User: " << user_id << ", ";
+        cout << "Served or not?: " << served_result;
+        if (is_served) { cout << ", " << "Departure Time: " << departure_time; }
+        cout << endl;
+    }
+
+    void serve_client(int departure = 0, bool served = false)
+    {
+        departure_time = departure;
+        is_served = served;
+    }
+
+    // Customer(int arrival, int service, int allowable_waiting)
+    //     : arrival_time(arrival),
+    //       service_time(service),
+    //       allowable_waiting_time(allowable_waiting){
+    //         cout << "arrival time: " << arrival_time << endl;
+    //         cout << "service time: " << service_time << endl;
+    //         cout << "allowable waiting time: " << allowable_waiting_time << endl;
+    //       };
+};
+
+#endif
